a02a997586f860f76329a48dab8062187a2a1b69
shen-web <9509394@qq.com>
2022-09-20 13:52:37 +0800

    chore: add postcss less and postcss config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: shen
+ * @Date: 2022-09-20 13:45:12
+ * @LastEditors: shen
+ * @LastEditTime: 2022-09-20 13:49:14
+ * @Description:
+ */
+module.exports = {
+	plugins: [require('postcss-preset-env')]
+}
