8c7db445921ba9eb3165c4e01bb9edd27e25f8db
shen-web <9509394@qq.com>
2022-09-26 09:35:03 +0800

    chore: global import antd styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@
  * @Author: shen
  * @Date: 2022-09-20 09:48:07
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 09:31:46
+ * @LastEditTime: 2022-09-26 09:34:43
  * @Description:
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+import 'antd/dist/antd.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
