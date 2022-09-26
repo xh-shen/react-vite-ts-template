d3123743eb35dff657d981f03e920a87811bdd73
shen-web <9509394@qq.com>
2022-09-26 15:48:53 +0800

    feat(utils): update utils export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-09-26 10:27:03
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 10:27:17
+ * @LastEditTime: 2022-09-26 15:37:32
  * @Description:
  */
 export * from './storage'
@@ -10,3 +10,4 @@ export * from './token'
 export * from './userInfo'
 export * from './constant'
 export * from './lang'
+export * from './antd'
