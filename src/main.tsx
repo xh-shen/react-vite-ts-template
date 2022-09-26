4764f16ea8ad976a20564909082bee69a58d7506
shen-web <9509394@qq.com>
2022-09-26 09:52:48 +0800

    chore: update the import of antd less to css

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@
  * @Author: shen
  * @Date: 2022-09-20 09:48:07
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 09:34:43
+ * @LastEditTime: 2022-09-26 09:51:52
  * @Description:
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import 'antd/dist/antd.less'
+import 'antd/dist/antd.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
