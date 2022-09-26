192d7e72e3038293e87cc4dbf6612f87bc9c9aa6
shen-web <9509394@qq.com>
2022-09-26 13:03:18 +0800

    chore: register store

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@
  * @Author: shen
  * @Date: 2022-09-20 09:48:07
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 09:51:52
+ * @LastEditTime: 2022-09-26 13:00:47
  * @Description:
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { store } from './store'
+import { Provider } from 'react-redux'
 import App from './App'
 
 import 'antd/dist/antd.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
+	// <React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>
+	</Provider>
+	// </React.StrictMode>
 )
