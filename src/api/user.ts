1f525dab0412d5f7b713dc930dc457b0a34c3494
shen-web <9509394@qq.com>
2022-09-26 15:54:59 +0800

    feat(api): update user login api

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,9 +2,10 @@
  * @Author: shen
  * @Date: 2022-09-26 09:24:38
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 09:24:46
+ * @LastEditTime: 2022-09-26 13:12:41
  * @Description:
  */
 import request from '@/request'
+import type { LoginResult, LoginParams } from '@/interfaces'
 
-export const login = (params: any) => request.post('/login', params)
+export const login = <T = LoginParams>(params: T) => request.post<LoginResult>('/login', params)
