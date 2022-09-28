0b6e4d07be2cc1e5e7499067ed47e5421c70f8b7
shen-web <9509394@qq.com>
2022-09-28 10:06:24 +0800

    feat(components): add all component export

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+/*
+ * @Author: shen
+ * @Date: 2022-09-28 08:25:21
+ * @LastEditors: shen
+ * @LastEditTime: 2022-09-28 08:32:42
+ * @Description:
+ */
+export { default as SvgIcon } from './svg-icon'
+export { default as SelectLang } from './select-lang'
