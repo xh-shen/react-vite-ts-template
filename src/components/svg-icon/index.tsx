f7bcb934e541b19224c8e19291d3aaea32e28525
shen-web <9509394@qq.com>
2022-09-28 10:04:23 +0800

    feat(components): update SvgIcon component dir to svg-icon

--- src/components/svg-icon/index.tsx
+++ src/components/svg-icon/index.tsx
@@ -2,20 +2,20 @@
  * @Author: shen
  * @Date: 2022-09-27 16:53:41
  * @LastEditors: shen
- * @LastEditTime: 2022-09-27 17:02:54
+ * @LastEditTime: 2022-09-28 08:29:50
  * @Description:
  */
 import type { FC } from 'react'
 
 import './index.less'
 
-type Props = {
+export type SvgIconProps = {
 	name: string
 	prefix?: string
 	color?: string
 }
 
-const SvgIcon: FC<Props> = ({ name, prefix = 'icon', color = '#333', ...props }) => {
+const SvgIcon: FC<SvgIconProps> = ({ name, prefix = 'icon', color = '#333', ...props }) => {
 	const symbolId = `#${prefix}-${name}`
 
 	return (
