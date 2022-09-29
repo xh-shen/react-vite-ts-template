a08bb63f4310cd2e82ee29432be2f0944316a97f
shen-web <9509394@qq.com>
2022-09-29 17:05:16 +0800

    feat(mock): add token check method

--- mock/_util.ts
+++ mock/_util.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-09-23 08:42:59
  * @LastEditors: shen
- * @LastEditTime: 2022-09-23 08:43:27
+ * @LastEditTime: 2022-09-29 13:25:13
  * @Description:
  */
 export interface Response<T = any> {
@@ -26,4 +26,15 @@ export function resultError(msg = 'Request failed', code = 500): Response {
 		data: null,
 		msg
 	}
+}
+
+export interface RequestParams {
+	method: string
+	body: any
+	headers?: { authorization?: string }
+	query: any
+}
+
+export function getRequestToken({ headers }: RequestParams): string | undefined {
+	return headers?.authorization
 }
