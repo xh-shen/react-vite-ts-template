3b3effb084eb098ad1d972355e8052702b665dd1
shen-web <9509394@qq.com>
2022-09-29 17:06:29 +0800

    feat: add antd config and router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@
  * @Author: shen
  * @Date: 2022-09-20 09:48:07
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 09:31:13
+ * @LastEditTime: 2022-09-29 16:41:06
  * @Description:
  */
 
-import type { FC } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import { useAntdLanguage } from './hooks'
+import Router from './router'
 
+import type { FC } from 'react'
 const App: FC = () => {
-	return <div>App</div>
+	const antdLanguage = useAntdLanguage()
+	return (
+		<BrowserRouter>
+			<ConfigProvider locale={antdLanguage}>
+				<Router />
+			</ConfigProvider>
+		</BrowserRouter>
+	)
 }
 
 export default App
