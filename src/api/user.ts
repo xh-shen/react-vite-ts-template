94bfe9f4bc24126a03178aa4e7e2e43439da16bf
shen-web <9509394@qq.com>
2022-09-29 16:15:01 +0800

    feat(api): add getUserInfo api

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,10 +2,12 @@
  * @Author: shen
  * @Date: 2022-09-26 09:24:38
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 13:12:41
+ * @LastEditTime: 2022-09-29 13:35:57
  * @Description:
  */
 import request from '@/request'
-import type { LoginResult, LoginParams } from '@/interfaces'
+import type { LoginResult, LoginParams, UserInfo } from '@/interfaces'
 
 export const login = <T = LoginParams>(params: T) => request.post<LoginResult>('/login', params)
+
+export const getUserInfo = () => request.get<UserInfo>('/getUserInfo')
