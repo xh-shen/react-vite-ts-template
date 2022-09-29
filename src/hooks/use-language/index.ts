396d4d1f227b63ae54eafb53e9d6483584ced45a
shen-web <9509394@qq.com>
2022-09-29 10:08:46 +0800

    feat(hooks): update use-language and add useAntdLanguage

--- src/hooks/use-language/index.ts
+++ src/hooks/use-language/index.ts
@@ -2,12 +2,16 @@
  * @Author: shen
  * @Date: 2022-09-28 09:22:28
  * @LastEditors: shen
- * @LastEditTime: 2022-09-28 10:51:06
+ * @LastEditTime: 2022-09-29 09:44:18
  * @Description:
  */
-
+import { useState, useEffect } from 'react'
 import { useAppDispatch, useAppSelector, setAppLang } from '@/store'
 import i18n from '@/locale'
+import zhCN from 'antd/lib/locale/zh_CN'
+import enUS from 'antd/lib/locale/en_US'
+
+import type { Locale } from 'antd/lib/locale-provider'
 
 export const useLanguage = (): [string, (language: string) => void] => {
 	const lang = useAppSelector(state => state.app.lang)
@@ -23,4 +27,20 @@ export const useLanguage = (): [string, (language: string) => void] => {
 	}
 
 	return [lang, setLanguage]
+}
+
+export const useAntdLanguage = () => {
+	const [language] = useLanguage()
+	const [antdLanguage, setAntdLanguage] = useState<Locale>()
+
+	const languageToAntdMap: Record<string, Locale> = {
+		en: enUS,
+		'zh-CN': zhCN
+	}
+
+	useEffect(() => {
+		setAntdLanguage(languageToAntdMap[language])
+	}, [language])
+
+	return antdLanguage
 }
