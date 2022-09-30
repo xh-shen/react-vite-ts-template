87a720362eb3d103b28b183171f5a85f1fed1f80
shen-web <9509394@qq.com>
2022-09-30 16:59:09 +0800

    feat(layout): add simple layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,19 +2,91 @@
  * @Author: shen
  * @Date: 2022-09-29 09:06:24
  * @LastEditors: shen
- * @LastEditTime: 2022-09-29 10:48:01
+ * @LastEditTime: 2022-09-30 16:32:27
  * @Description:
  */
-import type { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
+import { Breadcrumb, Layout, Menu, Button } from 'antd'
+import React from 'react'
 
-const Layout: FC = () => {
+import type { FC } from 'react'
+import { setAppInvalid, useAppDispatch } from '@/store'
+
+const { Header, Content, Sider } = Layout
+const items1 = ['1', '2', '3'].map(key => ({
+	key,
+	label: `nav ${key}`
+}))
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+	const key = String(index + 1)
+	return {
+		key: `sub${key}`,
+		icon: React.createElement(icon),
+		label: `subnav ${key}`,
+		children: new Array(4).fill(null).map((_, j) => {
+			const subKey = index * 4 + j + 1
+			return {
+				key: subKey,
+				label: `option${subKey}`
+			}
+		})
+	}
+})
+
+const BasicLayout: FC = () => {
+	const dispatch = useAppDispatch()
+
 	return (
-		<div>
-			Layout
-			<Outlet />
-		</div>
+		<Layout style={{ height: '100%' }}>
+			<Header className="header">
+				<div className="logo" />
+				<Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+				<Button onClick={() => dispatch(setAppInvalid(true))} style={{ position: 'absolute', right: 0, top: 10 }}>
+					退出
+				</Button>
+			</Header>
+			<Layout>
+				<Sider width={200} className="site-layout-background">
+					<Menu
+						mode="inline"
+						defaultSelectedKeys={['1']}
+						defaultOpenKeys={['sub1']}
+						style={{
+							height: '100%',
+							borderRight: 0
+						}}
+						items={items2}
+					/>
+				</Sider>
+				<Layout
+					style={{
+						padding: '0 24px 24px'
+					}}
+				>
+					<Breadcrumb
+						style={{
+							margin: '16px 0'
+						}}
+					>
+						<Breadcrumb.Item>Home</Breadcrumb.Item>
+						<Breadcrumb.Item>List</Breadcrumb.Item>
+						<Breadcrumb.Item>App</Breadcrumb.Item>
+					</Breadcrumb>
+					<Content
+						className="site-layout-background"
+						style={{
+							padding: 24,
+							margin: 0,
+							minHeight: 280
+						}}
+					>
+						<Outlet />
+					</Content>
+				</Layout>
+			</Layout>
+		</Layout>
 	)
 }
 
-export default Layout
+export default BasicLayout
