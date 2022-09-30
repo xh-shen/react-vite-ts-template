a06a589cfd87a182965a51c37b0963ddb0c27a81
shen-web <9509394@qq.com>
2022-09-30 16:58:28 +0800

    feat(pages): [error] add 404 page

---
+++ src/pages/error/NotFound.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: shen
+ * @Date: 2022-09-30 08:42:11
+ * @LastEditors: shen
+ * @LastEditTime: 2022-09-30 08:42:38
+ * @Description:
+ */
+function NotFound() {
+	return <div>404</div>
+}
+
+export default NotFound
