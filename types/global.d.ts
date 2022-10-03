aee87cc5c738a0d0c952c9605c4f74a672f7d30e
shen-web <9509394@qq.com>
2022-10-03 15:34:31 +0800

    feat(types): add global declare file

---
+++ types/global.d.ts
@@ -0,0 +1,53 @@
+/*
+ * @Author: shen
+ * @Date: 2022-10-03 10:13:10
+ * @LastEditors: shen
+ * @LastEditTime: 2022-10-03 14:59:36
+ * @Description:
+ */
+
+declare type Writable<T> = {
+	-readonly [P in keyof T]: T[P]
+}
+
+declare type Nullable<T> = T | null
+declare type Recordable<T = any> = Record<string, T>
+declare type ReadonlyRecordable<T = any> = {
+	readonly [key: string]: T
+}
+declare type Indexable<T = any> = {
+	[key: string]: T
+}
+declare type DeepPartial<T> = {
+	[P in keyof T]?: DeepPartial<T[P]>
+}
+declare type TimeoutHandle = ReturnType<typeof setTimeout>
+declare type IntervalHandle = ReturnType<typeof setInterval>
+
+declare interface ChangeEvent extends Event {
+	target: HTMLInputElement
+}
+
+declare interface WheelEvent {
+	path?: EventTarget[]
+}
+declare interface ImportMetaEnv extends ViteEnv {
+	__: unknown
+}
+
+declare interface ViteEnv {
+	VITE_PORT: number
+	VITE_USE_MOCK: boolean
+	VITE_PUBLIC_PATH: string
+	VITE_PROXY: [string, string][]
+	VITE_APP_TITLE: string
+	VITE_DROP_CONSOLE: boolean
+	VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none'
+	VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean
+	VITE_LEGACY: boolean
+	VITE_USE_IMAGEMIN: boolean
+	VITE_APP_THEME_COLOR: string
+	VITE_APP_LANGUAGE: 'en' | 'zh-CN'
+	VITE_API_URL_PREFIX: string
+	VITE_UPLOAD_URL_PREFIX: string
+}
