32724251d9d24d0b862a0b91cfb4d5ed201d58e8
shen-web <9509394@qq.com>
2022-10-03 15:44:01 +0800

    feat(config): update config based on environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-09-26 09:18:17
  * @LastEditors: shen
- * @LastEditTime: 2022-09-26 14:24:54
+ * @LastEditTime: 2022-10-03 14:59:43
  * @Description:
  */
 interface Config {
@@ -13,12 +13,14 @@ interface Config {
 	themeColor: string
 }
 
-const { themeColor, title, lang, baseApi } = JSON.parse('{}')
+console.log(import.meta.env)
+const { VITE_APP_TITLE, VITE_APP_LANGUAGE, VITE_APP_THEME_COLOR, VITE_API_URL_PREFIX } = import.meta.env
+
 const config: Config = {
-	title: title || 'shene react admin',
-	lang: lang || 'en',
-	baseApi: baseApi || '/api',
-	themeColor: themeColor || '#409eff',
+	title: VITE_APP_TITLE || 'Shene Admin',
+	lang: VITE_APP_LANGUAGE || 'en',
+	baseApi: VITE_API_URL_PREFIX || '/api',
+	themeColor: VITE_APP_THEME_COLOR || '#409eff',
 	timeout: 5000
 }
 
