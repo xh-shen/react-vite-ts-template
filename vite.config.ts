e936232d4b714d5bb494ed8609da82db51e706e1
shen-web <9509394@qq.com>
2022-10-06 15:07:39 +0800

    chore: add env and global import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-09-20 09:48:07
  * @LastEditors: shen
- * @LastEditTime: 2022-10-03 14:52:35
+ * @LastEditTime: 2022-10-06 14:32:00
  * @Description:
  */
 import { defineConfig, loadEnv } from 'vite'
@@ -27,7 +27,7 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 	const root = process.cwd()
 	const env = loadEnv(mode, root)
 	const viteEnv = parseEnv(env)
-	const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv
+	const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE, VITE_APP_NAMESPACE, VITE_APP_THEME_COLOR } = viteEnv
 	const isBuild = command === 'build'
 
 	return {
@@ -56,6 +56,20 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
 		define: {
 			__INTLIFY_PROD_DEVTOOLS__: false,
 			__APP_INFO__: JSON.stringify(__APP_INFO__)
+		},
+		css: {
+			preprocessorOptions: {
+				less: {
+					modifyVars: {
+						'primary-color': VITE_APP_THEME_COLOR,
+						namespace: VITE_APP_NAMESPACE
+					},
+					javascriptEnabled: true,
+					additionalData: `@import "@/styles/common/var.less";
+													 @import "@/styles/common/mixins.less";
+													`
+				}
+			}
 		},
 		plugins: createPlugins(viteEnv, isBuild)
 	}
