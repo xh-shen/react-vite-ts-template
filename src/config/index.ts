7ed7e5a20452b12596742b16bd827a0f59b35442
shen-web <9509394@qq.com>
2022-10-06 15:09:54 +0800

    feat(config): add namespace config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-09-26 09:18:17
  * @LastEditors: shen
- * @LastEditTime: 2022-10-03 14:59:43
+ * @LastEditTime: 2022-10-06 14:42:41
  * @Description:
  */
 interface Config {
@@ -11,16 +11,18 @@ interface Config {
 	baseApi: string
 	timeout: number
 	themeColor: string
+	namespace: string
 }
 
 console.log(import.meta.env)
-const { VITE_APP_TITLE, VITE_APP_LANGUAGE, VITE_APP_THEME_COLOR, VITE_API_URL_PREFIX } = import.meta.env
+const { VITE_APP_TITLE, VITE_APP_LANGUAGE, VITE_APP_THEME_COLOR, VITE_API_URL_PREFIX, VITE_APP_NAMESPACE } = import.meta.env
 
 const config: Config = {
 	title: VITE_APP_TITLE || 'Shene Admin',
 	lang: VITE_APP_LANGUAGE || 'en',
 	baseApi: VITE_API_URL_PREFIX || '/api',
 	themeColor: VITE_APP_THEME_COLOR || '#409eff',
+	namespace: VITE_APP_NAMESPACE || 'shene',
 	timeout: 5000
 }
 
