5b8b600fe1a52bba265867399f8a7bc078483175
shen-web <9509394@qq.com>
2022-10-15 22:39:54 +0800

    feat(store): update app module and add app setting

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -2,53 +2,46 @@
  * @Author: shen
  * @Date: 2022-09-26 10:50:37
  * @LastEditors: shen
- * @LastEditTime: 2022-10-14 13:34:15
+ * @LastEditTime: 2022-10-15 22:29:15
  * @Description:
  */
-import config from '@/config'
-import { getLang, setLang, getThemeColor, setThemeColor, getSettingValues, setSettingValues, getSettingValue } from '@/utils'
+import { getLang, setLang, getSettingValues, setSettingValues, getSettingValue, removeSettingValues } from '@/utils'
 import { createSlice } from '@reduxjs/toolkit'
-
+import defaultSetting from '@/defaultSetting'
+import config from '@/config'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import type { AppSetting, PageStyleType, LayoutType, CollapsePositionType } from '@/defaultSetting'
 
-export type PageStyleType = 'light' | 'dark' | 'realDark'
-export type LayoutType = 'side' | 'top' | 'mix'
-export type CollapsePositionType = 'top' | 'bottom' | 'none'
-
-export interface AppState {
+export interface AppState extends AppSetting {
 	lang: string
-	themeColor: string
 	authorized: boolean // 是否获取到授权信息
 	invalid: boolean // 是否登录失效
-	pageStyle: PageStyleType
-	layout: LayoutType
-	siderCollapsed: boolean
-	fixedHeader: boolean
-	fixSiderbar: boolean
-	siderWidth: number
-	headerHeight: number
-	colorWeak: boolean
-	grayMode: boolean
-	dragSidebar: boolean
-	collapsePosition: CollapsePositionType
 }
 
 const initialState: AppState = {
-	lang: getLang() ?? config.lang,
-	themeColor: getThemeColor() ?? config.themeColor,
 	authorized: false,
 	invalid: false,
-	pageStyle: getSettingValue<PageStyleType>('pageStyle') ?? 'light',
-	layout: getSettingValue<LayoutType>('layout') ?? 'side',
-	siderCollapsed: getSettingValue<boolean>('siderCollapsed') ?? false,
-	fixedHeader: getSettingValue<boolean>('fixedHeader') ?? true,
-	fixSiderbar: getSettingValue<boolean>('fixSiderbar') ?? true,
-	siderWidth: getSettingValue<number>('siderWidth') ?? 208,
-	headerHeight: getSettingValue<number>('headerHeight') ?? 48,
-	colorWeak: getSettingValue<boolean>('colorWeak') ?? config.colorWeak,
-	grayMode: getSettingValue<boolean>('grayMode') ?? config.grayMode,
-	dragSidebar: getSettingValue<boolean>('dragSidebar') ?? false,
-	collapsePosition: getSettingValue<CollapsePositionType>('collapsePosition') ?? 'top'
+	lang: getLang() ?? config.lang,
+	themeColor: getSettingValue<PageStyleType>('themeColor') ?? defaultSetting.themeColor,
+	pageStyle: getSettingValue<PageStyleType>('pageStyle') ?? defaultSetting.pageStyle,
+	layout: getSettingValue<LayoutType>('layout') ?? defaultSetting.layout,
+	siderCollapsed: getSettingValue<boolean>('siderCollapsed') ?? defaultSetting.siderCollapsed,
+	fixedHeader: getSettingValue<boolean>('fixedHeader') ?? defaultSetting.fixedHeader,
+	fixSiderbar: getSettingValue<boolean>('fixSiderbar') ?? defaultSetting.fixSiderbar,
+	siderWidth: getSettingValue<number>('siderWidth') ?? defaultSetting.siderWidth,
+	headerHeight: getSettingValue<number>('headerHeight') ?? defaultSetting.headerHeight,
+	colorWeak: getSettingValue<boolean>('colorWeak') ?? defaultSetting.colorWeak,
+	grayMode: getSettingValue<boolean>('grayMode') ?? defaultSetting.grayMode,
+	dragSidebar: getSettingValue<boolean>('dragSidebar') ?? defaultSetting.dragSidebar,
+	collapsePosition: getSettingValue<CollapsePositionType>('collapsePosition') ?? defaultSetting.collapsePosition,
+	showHeader: getSettingValue<boolean>('showHeader') ?? defaultSetting.showHeader,
+	showSiderbar: getSettingValue<boolean>('showSiderbar') ?? defaultSetting.showSiderbar,
+	showBreadcrumbs: getSettingValue<boolean>('showBreadcrumbs') ?? defaultSetting.showBreadcrumbs,
+	showLogo: getSettingValue<boolean>('showLogo') ?? defaultSetting.showLogo,
+	showFooter: getSettingValue<boolean>('showFooter') ?? defaultSetting.showFooter,
+	showCollapseButton: getSettingValue<boolean>('showCollapseButton') ?? defaultSetting.showCollapseButton,
+	fullContent: getSettingValue<boolean>('fullContent') ?? defaultSetting.fullContent,
+	accordionMenu: getSettingValue<boolean>('accordionMenu') ?? defaultSetting.accordionMenu
 }
 
 export const appSlice = createSlice({
@@ -58,10 +51,6 @@ export const appSlice = createSlice({
 		setAppLang(state, { payload }: PayloadAction<string>) {
 			state.lang = payload
 			setLang(payload)
-		},
-		setAppThemeColor(state, { payload }: PayloadAction<string>) {
-			state.themeColor = payload
-			setThemeColor(payload)
 		},
 		setAppAuthorized(state, { payload }: PayloadAction<boolean>) {
 			state.authorized = payload
@@ -76,11 +65,17 @@ export const appSlice = createSlice({
 			state[payload.key] = payload.value
 			const setting = getSettingValues() || {}
 			setSettingValues({ ...setting, [payload.key]: payload.value })
+		},
+		resetAppSetting(state) {
+			for (const key in defaultSetting) {
+				state[key] = defaultSetting[key]
+			}
+			removeSettingValues()
 		}
 	}
 })
 
-export const { setAppLang, setAppAuthorized, setAppInvalid, setAppThemeColor, setAppSettingValues, setSiderCollapsed } =
+export const { setAppLang, setAppAuthorized, setAppInvalid, setAppSettingValues, setSiderCollapsed, resetAppSetting } =
 	appSlice.actions
 
 export default appSlice.reducer
