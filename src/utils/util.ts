71ba80a8d205d1f6ad215aa2b404191e8b5d0de3
shen-web <9509394@qq.com>
2022-10-18 18:23:46 +0800

    feat(utils): util add stringify method

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -2,9 +2,24 @@
  * @Author: shen
  * @Date: 2022-09-30 14:22:04
  * @LastEditors: shen
- * @LastEditTime: 2022-09-30 14:22:06
+ * @LastEditTime: 2022-10-18 18:19:21
  * @Description:
  */
 export const sleep = async (delay: number) => {
 	return new Promise(resolve => setTimeout(resolve, delay))
+}
+
+export function stringify<K extends string | number | symbol, V>(obj: Record<K, V> | Map<K, V>) {
+	let tgt: Record<K, V>
+
+	if (obj instanceof Map) {
+		tgt = {} as any
+		obj.forEach((v, k) => {
+			tgt[k] = v
+		})
+	} else {
+		tgt = obj
+	}
+
+	return JSON.stringify(tgt)
 }
