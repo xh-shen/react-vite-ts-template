ba9252ee09ac6d0deff436e7aef4a811b6c1ab66
shen-web <9509394@qq.com>
2022-10-19 18:29:43 +0800

    feat(charts): add echart config

---
+++ src/charts/core.ts
@@ -0,0 +1,67 @@
+/*
+ * @Author: shen
+ * @Date: 2022-10-19 13:26:47
+ * @LastEditors: shen
+ * @LastEditTime: 2022-10-19 16:46:33
+ * @Description:
+ */
+
+import * as echarts from 'echarts/core'
+
+import { BarChart, LineChart, PieChart, MapChart, PictorialBarChart, RadarChart, ScatterChart } from 'echarts/charts'
+
+import {
+	TitleComponent,
+	TooltipComponent,
+	GridComponent,
+	PolarComponent,
+	AriaComponent,
+	ParallelComponent,
+	LegendComponent,
+	RadarComponent,
+	ToolboxComponent,
+	DataZoomComponent,
+	VisualMapComponent,
+	TimelineComponent,
+	CalendarComponent,
+	GraphicComponent
+} from 'echarts/components'
+
+import { SVGRenderer } from 'echarts/renderers'
+
+import type { EChartsOption } from 'echarts'
+import type { EChartsType } from 'echarts/core'
+
+export type { EChartsOption, EChartsType }
+
+echarts.use([
+	LegendComponent,
+	TitleComponent,
+	TooltipComponent,
+	GridComponent,
+	PolarComponent,
+	AriaComponent,
+	ParallelComponent,
+	BarChart,
+	LineChart,
+	PieChart,
+	MapChart,
+	RadarChart,
+	SVGRenderer,
+	PictorialBarChart,
+	RadarComponent,
+	ToolboxComponent,
+	DataZoomComponent,
+	VisualMapComponent,
+	TimelineComponent,
+	CalendarComponent,
+	GraphicComponent,
+	ScatterChart
+])
+
+export const createInstance = (dom: HTMLElement) => {
+	const instance: EChartsType = echarts.init(dom)
+	return instance
+}
+
+export default echarts
---
+++ src/charts/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: shen
+ * @Date: 2021-01-29 13:05:35
+ * @LastEditors: shen
+ * @LastEditTime: 2022-10-19 16:43:57
+ * @Description:
+ */
+import echarts from './core'
+export * from './core'
+export default echarts
