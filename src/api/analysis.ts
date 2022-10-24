3b88f502ca6cee074a2a9eccd27948c2e63dc623
shen-web <9509394@qq.com>
2022-10-24 15:28:17 +0800

    feat(api): add analysis page two row request api

--- src/api/analysis.ts
+++ src/api/analysis.ts
@@ -2,13 +2,22 @@
  * @Author: shen
  * @Date: 2022-10-23 15:30:19
  * @LastEditors: shen
- * @LastEditTime: 2022-10-23 16:12:02
+ * @LastEditTime: 2022-10-24 14:16:25
  * @Description:
  */
 import request from '@/request'
-import type { StatisticsSaleData, StatisticsVisitData, StatisticsPaymentData, StatisticsDealData } from '@/interfaces'
+import type {
+	StatisticsSaleData,
+	StatisticsVisitData,
+	StatisticsPaymentData,
+	StatisticsDealData,
+	StatisticsFlowTrendData,
+	StatisticsVisitTrendData
+} from '@/interfaces'
 
 export const getStatisticsSale = () => request.get<StatisticsSaleData>('/statistics/sale')
 export const getStatisticsVisit = () => request.get<StatisticsVisitData>('/statistics/visit')
 export const getStatisticsPayment = () => request.get<StatisticsPaymentData>('/statistics/payment')
 export const getStatisticsDeal = () => request.get<StatisticsDealData>('/statistics/deal')
+export const getStatisticsFlowTrend = () => request.get<{ list: StatisticsFlowTrendData }>('/statistics/flowTrend')
+export const getStatisticsVisitTrend = () => request.get<{ list: StatisticsVisitTrendData }>('/statistics/visitTrend')
