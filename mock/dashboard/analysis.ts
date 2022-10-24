dbf8e48b5270078e954ee788783273d3dc1074f3
shen-web <9509394@qq.com>
2022-10-24 15:25:20 +0800

    feat(mock): finish analysis tow row mock data

--- mock/dashboard/analysis.ts
+++ mock/dashboard/analysis.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-10-23 15:22:37
  * @LastEditors: shen
- * @LastEditTime: 2022-10-23 16:14:25
+ * @LastEditTime: 2022-10-24 14:46:17
  * @Description:
  */
 import Mock from 'mockjs'
@@ -83,6 +83,43 @@ export default [
 						}
 					],
 					'totalVisit|100000-200000': 200000
+				})
+			)
+		}
+	},
+	{
+		url: '/api/statistics/flowTrend',
+		method: 'get',
+		response: request => {
+			const token = getRequestToken(request)
+			if (!token) return resultError('Invalid token', 401)
+			return resultSuccess<any>(
+				Mock.mock({
+					'list|24': [
+						{
+							'value1|10000-60000': 60000,
+							'value2|1000-10000': 10000
+						}
+					]
+				})
+			)
+		}
+	},
+	{
+		url: '/api/statistics/visitTrend',
+		method: 'get',
+		response: request => {
+			const token = getRequestToken(request)
+			if (!token) return resultError('Invalid token', 401)
+			return resultSuccess<any>(
+				Mock.mock({
+					'list|12': [
+						{
+							'value1|10000-80000': 80000,
+							'value2|1000-40000': 10000,
+							'value3|5000-60000': 60000
+						}
+					]
 				})
 			)
 		}
