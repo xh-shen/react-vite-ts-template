76cf3cb4e47fbafdd2e9bc0e68ff89914d205f8e
shen-web <9509394@qq.com>
2022-10-25 15:44:37 +0800

    feat(components): update page-container

--- src/components/page-container/index.tsx
+++ src/components/page-container/index.tsx
@@ -2,21 +2,23 @@
  * @Author: shen
  * @Date: 2022-10-18 18:33:21
  * @LastEditors: shen
- * @LastEditTime: 2022-10-18 18:39:05
+ * @LastEditTime: 2022-10-25 10:14:02
  * @Description:
  */
 import { usePrefixCls } from '@/hooks'
 import './index.less'
 
-import type { FC, ReactNode } from 'react'
+import type { FC, ReactNode, CSSProperties } from 'react'
 
 export interface PageContainerProps {
+	className?: string
+	style?: CSSProperties
 	children: ReactNode
 }
 
-const PageContainer: FC<PageContainerProps> = ({ children }) => {
+const PageContainer: FC<PageContainerProps> = ({ children, className }) => {
 	const prefixCls = usePrefixCls('page-container')
-	return <div className={prefixCls}>{children}</div>
+	return <div className={`${prefixCls} ${className}`}>{children}</div>
 }
 
 export default PageContainer
