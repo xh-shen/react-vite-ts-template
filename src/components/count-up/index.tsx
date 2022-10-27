e2f4dbf3a096d04d0a53fa55d5d5ffdc2709dd0b
shen-web <9509394@qq.com>
2022-10-27 13:49:00 +0800

    feat(components): update count-up component directory structure

--- src/components/count-up/index.tsx
+++ src/components/count-up/index.tsx
@@ -2,9 +2,9 @@
  * @Author: shen
  * @Date: 2022-10-24 08:01:40
  * @LastEditors: shen
- * @LastEditTime: 2022-10-24 08:35:16
+ * @LastEditTime: 2022-10-27 10:18:15
  * @Description:
  */
-export { default } from './src/CountUp'
-export type { CountUpProps } from './src/CountUp'
-export { default as useCountUp } from './src/useCountUp'
+export { default } from './CountUp'
+export type { CountUpProps } from './CountUp'
+export { default as useCountUp } from './useCountUp'
