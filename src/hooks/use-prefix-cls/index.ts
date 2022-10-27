0e467737398932d8737b3ecc932e7b9e9b5f11d3
shen-web <9509394@qq.com>
2022-10-27 13:50:47 +0800

    feat(hooks): update use-prefix-cls hook

--- src/hooks/use-prefix-cls/index.ts
+++ src/hooks/use-prefix-cls/index.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2022-10-06 19:59:35
  * @LastEditors: shen
- * @LastEditTime: 2022-10-06 21:18:22
+ * @LastEditTime: 2022-10-26 15:06:33
  * @Description:
  */
 
@@ -17,3 +17,5 @@ export const usePrefixCls = (block: string) => {
 	const { namespace } = config
 	return getPrefixCls(namespace, block)
 }
+
+export const rootPrefixCls = config.namespace
