616e88157447c9fe5792a63c2a7395ddc5e444a3
shen-web <9509394@qq.com>
2022-10-29 11:57:34 +0800

    feat(pages): init table pages

---
+++ src/pages/table/basic/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: shen
+ * @Date: 2022-09-29 09:03:52
+ * @LastEditors: shen
+ * @LastEditTime: 2022-10-29 11:54:36
+ * @Description:
+ */
+import { Card } from 'antd'
+import { PageContainer } from '@/components'
+
+import type { FC } from 'react'
+const BasicTable: FC = () => {
+	return (
+		<PageContainer>
+			<Card title="基础表格">基础表格</Card>
+		</PageContainer>
+	)
+}
+
+export default BasicTable
---
+++ src/pages/table/query/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: shen
+ * @Date: 2022-09-29 09:03:52
+ * @LastEditors: shen
+ * @LastEditTime: 2022-10-29 11:55:14
+ * @Description:
+ */
+import { Card } from 'antd'
+import { PageContainer } from '@/components'
+
+import type { FC } from 'react'
+const QueryTable: FC = () => {
+	return (
+		<PageContainer>
+			<Card title="查询表格">查询表格</Card>
+		</PageContainer>
+	)
+}
+
+export default QueryTable
